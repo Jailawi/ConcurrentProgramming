6fb5ea5fac824485f73e47b0d1623bc75f5f3a93
Dodo <Dodo@LAPTOP-S32755C9>
2020-09-08 09:27:33 +0200

    timeTicking in ClockThread works now

--- clock/src/clock/io/ClockThread.java
+++ clock/src/clock/io/ClockThread.java
@@ -21,62 +21,31 @@ public class ClockThread extends Thread {
 	public void timeTicking() {
 		long t0 = System.currentTimeMillis();
 		try {
-			
-		
-		while(true) {
-			long now = System.currentTimeMillis();
-			increaseTime();
-			out.displayTime(h, m, s);
-			Thread.sleep((t0 + ((s + 1) * 1000)) - now);
-		}
-		} catch (InterruptedException e) {
-			throw new Error(e);
-		}
-		
-	}
-	
-	
-	public void increaseTime() {
-		s++;
-		if(s==59) {
-			m++;
-			if(m==59) {
-				h++;
-			}
-		}
-		
-	}
-		
-		
-		
-		/*
-		try {
 
-			long t0 = System.currentTimeMillis();
-			int sec=0;
-			while(sec<59){
+			int sec = 0;
+			while (true) {
 				long now = System.currentTimeMillis();
-				out.displayTime(h, m, s += 1);
+				increaseTime();
+				out.displayTime(h, m, s);
 				Thread.sleep((t0 + ((sec + 1) * 1000)) - now);
 				sec++;
-				if (s == 59) {
-					System.out.print("here");
-					s = 0;
-					out.displayTime(h, m += 1, s);
-					if (m == 59) {
-						m = 0;
-						s = 0;
-						out.displayTime(h += 1, m, s);
-
-					}
-				}
 			}
 		} catch (InterruptedException e) {
 			throw new Error(e);
 		}
-		
-		*/
 
 	}
 
+	public void increaseTime() {
+		s++;
+		if (s > 59) {
+			m++;
+			s = 0;
+			if (m > 59) {
+				h++;
+				m = 0;
+			}
+		}
 
+	}
+}
