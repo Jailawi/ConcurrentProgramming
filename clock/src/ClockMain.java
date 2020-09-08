1170ce802da03bf870c48b2008c404718704d785
Musti boii <mustafa.al-jailawi@mail.com>
2020-09-08 16:07:05 +0200

    new tings

--- clock/src/ClockMain.java
+++ clock/src/ClockMain.java
@@ -1,41 +1,34 @@
 import clock.AlarmClockEmulator;
+import clock.io.Clock;
 import clock.io.ClockInput;
 import clock.io.ClockInput.UserInput;
 import clock.io.ClockOutput;
-import clock.io.ClockThread;
 
 public class ClockMain {
 
     public static void main(String[] args) throws InterruptedException {
         AlarmClockEmulator emulator = new AlarmClockEmulator();
 
-        ClockInput  in  = emulator.getInput();
+        ClockInput in = emulator.getInput();
         ClockOutput out = emulator.getOutput();
-        
-        ClockThread thread= new ClockThread(out,in);
+
+        Clock clock = new Clock(out, in);
+        Thread thread = new Thread(clock);
         thread.start();
 
-
-        
-        
-  
         while (true) {
-        	in.getSemaphore().acquire();
+            in.getSemaphore().acquire();
             UserInput userInput = in.getUserInput();
             int choice = userInput.getChoice();
-            int h=userInput.getHours();
+            int h = userInput.getHours();
             int m = userInput.getMinutes();
-            int s= userInput.getSeconds();
-            
+            int s = userInput.getSeconds();
 
-
-            if(choice == 1) {
-            thread.time.getCurrentTime().parseUserInput(h,m,s);
+            if (choice == 1) {
+                clock.setTime(h, m, s);
             }
 
-           
-            
-          //  System.out.println("choice=" + choice + " h=" + h + " m=" + m + " s=" + s);
+            // System.out.println("choice=" + choice + " h=" + h + " m=" + m + " s=" + s);
         }
 
     }
--- clock/src/clock/io/Time.java
+++ clock/src/clock/io/Time.java
@@ -2,54 +2,47 @@ package clock.io;
 
 import java.util.concurrent.Semaphore;
 
-import clock.io.ClockInput.UserInput;
-
 public class Time {
-	private int h,m,s;
+	private int h, m, s;
 	private Semaphore mutex = new Semaphore(1);
-	
-	
+
 	public Time(int h, int m, int s) {
-		this.h=h;
-		this.s=s;
-		this.m=m;
+		this.h = h;
+		this.s = s;
+		this.m = m;
 	}
-	
+
 	public Time getCurrentTime() {
 		return this;
 	}
-	
+
 	public int getHour() {
 		return h;
 	}
-	
+
 	public int getMin() {
 		return m;
 	}
-	
+
 	public int getSec() {
 		return s;
 	}
-	
+
 	/*
-	public void getTimeFromUser(UserInput userInput) throws InterruptedException {
+	 * public void getTimeFromUser(UserInput userInput) throws InterruptedException
+	 * { mutex.acquire(); h= userInput.getHours(); m= userInput.getMinutes(); s=
+	 * userInput.getSeconds(); mutex.release(); }
+	 * 
+	 */
+
+	public void setTime(int h, int m, int s) throws InterruptedException {
 		mutex.acquire();
-		h= userInput.getHours();
-		m= userInput.getMinutes();
-		s= userInput.getSeconds();
+		this.h = h;
+		this.m = m;
+		this.s = s;
 		mutex.release();
 	}
-	
-	*/
-	
-	public void parseUserInput(int h, int m, int s) throws InterruptedException {
-		mutex.acquire();
-		this.h=h;
-		this.m=m;
-		this.s=s;
-		mutex.release();
-	}
-	
+
 	public void increaseTime() throws InterruptedException {
 		mutex.acquire();
 		s++;
@@ -63,5 +56,5 @@ public class Time {
 		}
 		mutex.release();
 	}
-	
+
 }
