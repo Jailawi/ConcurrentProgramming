66795e479ff2fd7a9ff97d6e5a719cff265dd8fc
Dodo <Dodo@LAPTOP-S32755C9>
2020-09-08 09:07:17 +0200

    Fixed the clock tick error, now it ticks every sec

--- clock/src/clock/io/ClockThread.java
+++ clock/src/clock/io/ClockThread.java
@@ -19,13 +19,46 @@ public class ClockThread extends Thread {
 	}
 
 	public void timeTicking() {
+		long t0 = System.currentTimeMillis();
+		try {
+			
+		
+		while(true) {
+			long now = System.currentTimeMillis();
+			increaseTime();
+			out.displayTime(h, m, s);
+			Thread.sleep((t0 + ((s + 1) * 1000)) - now);
+		}
+		} catch (InterruptedException e) {
+			throw new Error(e);
+		}
+		
+	}
+	
+	
+	public void increaseTime() {
+		s++;
+		if(s==59) {
+			m++;
+			if(m==59) {
+				h++;
+			}
+		}
+		
+	}
+		
+		
+		
+		/*
 		try {
 
 			long t0 = System.currentTimeMillis();
-			for (int sec = 0; sec <= 59; sec++) {
+			int sec=0;
+			while(sec<59){
 				long now = System.currentTimeMillis();
 				out.displayTime(h, m, s += 1);
 				Thread.sleep((t0 + ((sec + 1) * 1000)) - now);
+				sec++;
 				if (s == 59) {
 					System.out.print("here");
 					s = 0;
@@ -41,7 +74,9 @@ public class ClockThread extends Thread {
 		} catch (InterruptedException e) {
 			throw new Error(e);
 		}
+		
+		*/
 
 	}
 
-}
+
