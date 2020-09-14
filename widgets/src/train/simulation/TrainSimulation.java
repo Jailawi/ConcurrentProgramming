e7c07680c1c0de18ba5cd8c9ecd26b8cac4479dc
Dodo <Dodo@LAPTOP-S32755C9>
2020-09-14 18:29:56 +0200

    One train is now able to move

--- widgets/src/train/simulation/TrainSimulation.java
+++ widgets/src/train/simulation/TrainSimulation.java
@@ -1,20 +1,37 @@
 package train.simulation;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import train.model.Route;
 import train.model.Segment;
 import train.view.TrainView;
 
 public class TrainSimulation {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		List<Segment> train = new LinkedList<>();
 
-        TrainView view = new TrainView();
-        
-        Route route = view.loadRoute();
-        
-        Segment first = route.next();
-        
-        first.enter();
-    }
+		TrainView view = new TrainView();
+
+		Route route = view.loadRoute();
+
+		for (int i = 0; i < 3; i++) {
+			train.add(route.next());
+		}
+
+		for (int i = 0; i < 3; i++) {
+			train.get(i).enter();
+		}
+
+		while (true) {
+			Segment head = route.next();
+			train.add(0, head);
+			head.enter();
+			Segment tail = train.remove(train.size() - 1);
+			tail.exit();
+		}
+
+	}
 
 }
