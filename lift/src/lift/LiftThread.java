93f5de61ec041ab2f8b40ad740eedfbb69231bd9
Dodo <Dodo@LAPTOP-S32755C9>
2020-09-22 16:20:23 +0200

    Khalas vi är klara XD

--- lift/src/lift/LiftThread.java
+++ lift/src/lift/LiftThread.java
@@ -6,64 +6,25 @@ public class LiftThread implements Runnable {
 	private int currentFloor = 0;
 	private int next = 1;
 	private int nextFloor = currentFloor + next;
-	private boolean stop, stop1, stop2 = false;
+
 
 	public LiftThread(LiftView lift, Monitor m) {
 		this.lift = lift;
 		this.m = m;
 	}
 
-	private synchronized void waitOutside() throws InterruptedException {
-		wait(1250);
-	}
+	
 
 	@Override
 	public void run() {
+	
 		while (true) {
 
-			for (int i = 0; i <= 5; i++) {
-				lift.moveLift(currentFloor, currentFloor + 1);
-				currentFloor++;
-				lift.openDoors(currentFloor);
-				stop1 = m.checkExiting(currentFloor);
-				stop2 = m.checkEntering(currentFloor);
-				if (stop1 || stop2) {
-					try {
-						waitOutside();
-						stop1 = false;
-						stop2 = false;
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-
-				lift.closeDoors();
-
-			}
-
-			for (int i = 5; i >= 0; i--) {
-				// System.out.println("this should be false: " + stop);
-				lift.moveLift(currentFloor, currentFloor - 1);
-				currentFloor--;
-				System.out.println(currentFloor);
-				lift.openDoors(currentFloor);
-				// checkExiting(currentFloor);
-				stop1 = m.checkExiting(currentFloor);
-				stop2 = m.checkEntering(currentFloor);
-				if (stop1 || stop2) {
-					try {
-						waitOutside();
-						stop1 = false;
-						stop2 = false;
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				lift.closeDoors();
-			}
+			m.moveUp();
+			m.moveDown();
+		
 		}
 	}
-
+	
+	
 }
--- lift/src/lift/Monitor.java
+++ lift/src/lift/Monitor.java
@@ -1,11 +1,8 @@
 package lift;
 
-import java.util.concurrent.locks.ReentrantLock;
 
-import jdk.javadoc.doclet.Reporter;
 
 public class Monitor {
-	// int floor; // the floor the lift is currently on
 	boolean moving = true; // true if the lift is moving, false if standing still with doors open
 	int direction; // +1 if lift is going up, -1 if going down
 	int[] waitEntry; // number of passengers waiting to enter the lift at the various floors
@@ -14,32 +11,46 @@ public class Monitor {
 	LiftView view;
 	int currentFloor = 0;
 	boolean stopLift = false;
+	private boolean stop, stop1, stop2 = false;
+	private boolean doorOpen;
 
 	public Monitor(LiftView view) {
 		waitEntry = new int[7];
 		waitExit = new int[7];
 		this.view = view;
 	}
+	
+	public synchronized int getNbrOfExiting(int floor) {
+		return waitExit[floor];
+	}
+	
+	public synchronized int getNbrOfEntering(int floor) {
+		return waitEntry[floor];
+	}
 
-	private synchronized void reportPassengerEntered(int fromFloor) {
+	private synchronized void reportPassengerEnteredLift(int fromFloor) {
+
 		waitEntry[fromFloor] -= 1;
 		notifyAll();
 	}
 
-	private synchronized void reportPassengerExited(int fromFloor) {
-		waitExit[fromFloor] = waitEntry[fromFloor] -= 1;
+	private synchronized void reportPassengerExitedLift(int fromFloor) {
+		waitExit[fromFloor] = waitExit[fromFloor] -= 1;
 		notifyAll();
 	}
 
 	public synchronized void setPassengerTravel(int fromFloor, int toFloor, Passenger pass)
 			throws InterruptedException {
+
 		waitEntry[fromFloor] += 1;
 
-		while (fromFloor != currentFloor || load == 4) {
+
+		while (fromFloor != currentFloor || load == 4 || !doorOpen) {
 			wait();
 		}
-		pass.enterLift();
 		waitExit[toFloor] += 1;
+
+		pass.enterLift();
 		load++;
 		while (currentFloor != toFloor) {
 			wait();
@@ -48,26 +59,109 @@ public class Monitor {
 		load--;
 
 	}
+	
 
 	public synchronized boolean checkEntering(int currentFloor) {
 		// System.out.println(waitEntry[currentFloor]);
+		view.showDebugInfo(waitEntry, waitExit);
+
 		if (waitEntry[currentFloor] > 0) {
 			this.currentFloor = currentFloor;
-			reportPassengerEntered(currentFloor);
+			reportPassengerEnteredLift(currentFloor);
 			return true;
 		}
 		return false;
 	}
 
 	public synchronized boolean checkExiting(int currentFloor) {
-		// System.out.println(waitExit[currentFloor]);
+
+		 System.out.println(waitExit[currentFloor]);
 		if (waitExit[currentFloor] > 0) {
 			System.out.println("passenger should exit here");
 			this.currentFloor = currentFloor;
-			reportPassengerExited(currentFloor);
+			reportPassengerExitedLift(currentFloor);
 			return true;
 		}
 		return false;
 	}
+	
+	private synchronized void waitOutside(int x) throws InterruptedException {
+		wait(1250 * x);
+	}
+	
+	
+	public void moveUp() {
+		for (int i = 0; i <= 5; i++) {
+			view.moveLift(currentFloor, currentFloor + 1);
+			currentFloor++;
+			view.openDoors(currentFloor);
+			doorOpen=true;
+			stop1 = checkExiting(currentFloor);
+			stop2 = checkEntering(currentFloor);
+			if (stop1) {
+				try {
+					waitOutside(waitExit[currentFloor]+1);
+					stop1 = false;
+					stop2=false;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}else if(stop2) {
+				try {
+					waitOutside(waitEntry[currentFloor]+1);
+					stop2 = false;
+					stop1= false;
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+			doorOpen=false;
+			view.closeDoors();
+
+		}
+		
+	}
+	
+	
+	public void moveDown() {
+	for (int i = 5; i >= 0; i--) {
+		// System.out.println("this should be false: " + stop);
+		view.moveLift(currentFloor, currentFloor - 1);
+		currentFloor--;
+		//System.out.println(currentFloor);
+		view.openDoors(currentFloor);
+		doorOpen=true;
+		// checkExiting(currentFloor);
+		stop1 = checkExiting(currentFloor);
+		stop2 = checkEntering(currentFloor);
+		if (stop1) {
+			try {
+				System.out.println("waitexit: " +waitExit[currentFloor]);
+				waitOutside(waitExit[currentFloor]+1);
+				stop1 = false;
+				stop2=false;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}else if(stop2) {
+			try {
+				waitOutside(waitEntry[currentFloor]+1);
+				stop1=false;
+				stop2 = false;
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		doorOpen=false;
+		view.closeDoors();
+	}
+	
+}
+
 
 }
