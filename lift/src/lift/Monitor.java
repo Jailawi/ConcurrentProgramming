fafe98b2a3b59639a1a9dd8e8b771868acf01b39
Musti boii <mustafa.al-jailawi@mail.com>
2020-09-23 21:37:56 +0200

    timing left?

--- lift/src/lift/Monitor.java
+++ lift/src/lift/Monitor.java
@@ -1,5 +1,7 @@
 package lift;
 
+import java.util.Arrays;
+
 public class Monitor {
 	boolean moving = true; // true if the lift is moving, false if standing still with doors open
 	int direction; // +1 if lift is going up, -1 if going down
@@ -26,11 +28,15 @@ public class Monitor {
 
 	private synchronized void reportPassengerEnteredLift(int fromFloor) {
 		waitEntry[fromFloor] -= 1;
+
+		// System.out.println("Wants to enter " + Arrays.toString(waitEntry));
 		notifyAll();
 	}
 
 	private synchronized void reportPassengerExitedLift(int fromFloor) {
-		waitExit[fromFloor] = waitExit[fromFloor] -= 1;
+		waitExit[fromFloor] = 0;
+		// System.out.println("Wants to exit " + Arrays.toString(waitExit));
+
 		notifyAll();
 	}
 
@@ -41,7 +47,9 @@ public class Monitor {
 			wait();
 		}
 		waitExit[toFloor] += 1;
+
 		load++;
+		view.showDebugInfo(waitEntry, waitExit);
 	}
 
 	public synchronized void exitPassengerFromLift(int fromFloor, int toFloor, Passenger pass)
@@ -55,11 +63,13 @@ public class Monitor {
 	}
 
 	public synchronized boolean checkEntering(int currentFloor) {
-		// System.out.println(waitEntry[currentFloor]);
-		view.showDebugInfo(waitEntry, waitExit);
+		// view.showDebugInfo(waitEntry, waitExit);
 		if (waitEntry[currentFloor] > 0) {
 			this.currentFloor = currentFloor;
-			reportPassengerEnteredLift(currentFloor);
+			for (var i = 1; i <= waitEntry[currentFloor]; i++) {
+				reportPassengerEnteredLift(currentFloor);
+			}
+
 			return true;
 		}
 		return false;
@@ -71,7 +81,10 @@ public class Monitor {
 		if (waitExit[currentFloor] > 0) {
 			// System.out.println("passenger should exit here");
 			this.currentFloor = currentFloor;
+			System.out.println("Wants to exit: " + waitExit[currentFloor]);
+			// for (var i = 1; i <= waitExit[currentFloor]; i++) {
 			reportPassengerExitedLift(currentFloor);
+			// }
 			return true;
 		}
 		return false;
@@ -81,89 +94,102 @@ public class Monitor {
 		wait(1250 * x);
 	}
 
+	private synchronized void concurrentWalkers() {
+		// numberOfConcurrentlyWalking = waitExit[currentFloor] + 4 - (load -
+		// waitExit[currentFloor]);
+		int emptySpace = 4 - load;
+		int willEnter = 0;
+		if (waitEntry[currentFloor] > emptySpace) {
+			willEnter = emptySpace;
+		} else if (waitEntry[currentFloor] < emptySpace) {
+			willEnter = waitEntry[currentFloor];
+		} else {
+			willEnter = emptySpace;
+		}
+		numberOfConcurrentlyWalking = waitExit[currentFloor] + willEnter;
+		System.out.println("CurrentWalkers " + numberOfConcurrentlyWalking);
+	}
 
 	public void checkCondition() {
 		stop1 = checkExiting(currentFloor);
 		stop2 = checkEntering(currentFloor);
-		
-		numberOfConcurrentlyWalking=waitExit[currentFloor] + 4-(load-waitExit[currentFloor]);
 
-		if (stop1&&stop2) {
+		concurrentWalkers();
+
+		if (stop1 && stop2) {
 			try {
-				if(doorOpen) {
-				waitOutside((numberOfConcurrentlyWalking)+1);
-				
-				stop1 = false;
-				stop2=false;
+				if (doorOpen) {
+					waitOutside((numberOfConcurrentlyWalking) + 1);
+
+					stop1 = false;
+					stop2 = false;
 				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(stop2) {
+		} else if (stop2) {
 			try {
-				if(doorOpen) {
-				waitOutside(numberOfConcurrentlyWalking+1);
-				stop1=false;
-				stop2 = false;
+				if (doorOpen) {
+					waitOutside(numberOfConcurrentlyWalking + 1);
+					stop1 = false;
+					stop2 = false;
 				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-		}else if(stop1) {
+		} else if (stop1) {
 			try {
-				if(doorOpen) {
-				waitOutside(numberOfConcurrentlyWalking+1);
-				stop2 = false;
-				stop1= false;
+				if (doorOpen) {
+					waitOutside(numberOfConcurrentlyWalking + 1);
+					stop2 = false;
+					stop1 = false;
 				}
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		
+
 	}
-	
-	
+
 	public void moveUp() {
 		for (int i = 0; i <= 5; i++) {
 			view.moveLift(currentFloor, currentFloor + 1);
 			currentFloor++;
-			if(waitEntry[currentFloor] >=1 && load <=3 || waitExit[currentFloor] >=1) {
-			
-			view.openDoors(currentFloor);
-			doorOpen=true;
+			if (waitEntry[currentFloor] >= 1 && load <= 3 || waitExit[currentFloor] >= 1) {
+
+				view.openDoors(currentFloor);
+				doorOpen = true;
 			}
-		
-			checkCondition();	
-			if(doorOpen) {
-				doorOpen=false;
+
+			checkCondition();
+			if (doorOpen) {
+				doorOpen = false;
 				view.closeDoors();
-				}
-		}
-		
-	}
-	
-	
-	public void moveDown() {
-	for (int i = 5; i >= 0; i--) {
-		view.moveLift(currentFloor, currentFloor - 1);
-		currentFloor--;
-	
-		if(waitEntry[currentFloor] >=1 && load <=3 || waitExit[currentFloor] >=1) {
-			view.openDoors(currentFloor);
-			doorOpen=true;
 			}
-				
-		checkCondition();
-		if(doorOpen) {
-		doorOpen=false;
-		view.closeDoors();
 		}
+
 	}
-	
-}
+
+	public void moveDown() {
+		for (int i = 5; i >= 0; i--) {
+			view.moveLift(currentFloor, currentFloor - 1);
+			currentFloor--;
+
+			if (waitEntry[currentFloor] >= 1 && load <= 3 || waitExit[currentFloor] >= 1) {
+				view.openDoors(currentFloor);
+				doorOpen = true;
+			}
+
+			checkCondition();
+			if (doorOpen) {
+				doorOpen = false;
+				view.closeDoors();
+			}
+		}
+
+	}
 
 }
