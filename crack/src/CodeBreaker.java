d3ee56a6143952309099e2813b9deb5edda6e08a
Dodo <Dodo@LAPTOP-S32755C9>
2020-09-28 18:07:38 +0200

    l5 finito

--- crack/src/CodeBreaker.java
+++ crack/src/CodeBreaker.java
@@ -11,56 +11,80 @@ import client.view.StatusWindow;
 import client.view.WorklistItem;
 import network.Sniffer;
 import network.SnifferCallback;
+import rsa.Factorizer;
+import rsa.ProgressTracker;
+
+import rsa.Factorizer;
+import rsa.ProgressTracker;
 
 public class CodeBreaker implements SnifferCallback {
 
-    private final JPanel workList;
-    private final JPanel progressList;
-    
-    private final JProgressBar mainProgressBar;
+	private final JPanel workList;
+	private final JPanel progressList;
 
-    // -----------------------------------------------------------------------
-    
-    private CodeBreaker() {
-        StatusWindow w  = new StatusWindow();
+	private final JProgressBar mainProgressBar;
 
-        workList        = w.getWorkList();
-        progressList    = w.getProgressList();
-        mainProgressBar = w.getProgressBar();
-    }
-    
-    // -----------------------------------------------------------------------
-    
-    public static void main(String[] args) {
-    
-        /*
-         * Most Swing operations (such as creating view elements) must be performed in
-         * the Swing EDT (Event Dispatch Thread).
-         * 
-         * That's what SwingUtilities.invokeLater is for.
-         */
+	// -----------------------------------------------------------------------
 
-        SwingUtilities.invokeLater(() -> {
-            CodeBreaker codeBreaker = new CodeBreaker();
-            new Sniffer(codeBreaker).start();
-            
-        });
-    }
+	private CodeBreaker() {
+		StatusWindow w = new StatusWindow();
 
-    // -----------------------------------------------------------------------
+		workList = w.getWorkList();
+		progressList = w.getProgressList();
+		mainProgressBar = w.getProgressBar();
+	}
 
-    /** Called by a Sniffer thread when an encrypted message is obtained. */
-    @Override
-    public void onMessageIntercepted(String message, BigInteger n) {
-    	WorklistItem workItem= new WorklistItem(n, message);
-    	workList.add(workItem);
-    	
-    	workItem.getButton().addActionListener(e -> {
-    		workList.remove(workItem);
-    		ProgressItem progressItem= new ProgressItem(n, message);
-    		progressList.add(progressItem);
-    	});
-    
-       // System.out.println("message intercepted (N=" + n + ")...");
-    }
+	// -----------------------------------------------------------------------
+
+	public static void main(String[] args) {
+
+		/*
+		 * Most Swing operations (such as creating view elements) must be performed in
+		 * the Swing EDT (Event Dispatch Thread).
+		 * 
+		 * That's what SwingUtilities.invokeLater is for.
+		 */
+
+		SwingUtilities.invokeLater(() -> {
+			CodeBreaker codeBreaker = new CodeBreaker();
+			new Sniffer(codeBreaker).start();
+
+		});
+	}
+
+	// -----------------------------------------------------------------------
+
+	/** Called by a Sniffer thread when an encrypted message is obtained. */
+	@Override
+	public void onMessageIntercepted(String message, BigInteger n) {
+		WorklistItem workItem = new WorklistItem(n, message);
+		workList.add(workItem);
+
+		workItem.getButton().addActionListener(e -> {
+
+			try {
+				workList.remove(workItem);
+				ProgressItem progressItem = new ProgressItem(n, message);
+				progressList.add(progressItem);
+				ProgressTracker tracker = new Tracker();
+				String plaintext = Factorizer.crack(message, n, tracker);
+				System.out.println("da message for da beople: " + plaintext);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
+
+		// System.out.println("message intercepted (N=" + n + ")...");
+	}
+}
+
+class Tracker implements ProgressTracker {
+	private int totalProgress = 0;
+
+	@Override
+	public void onProgress(int ppmDelta) {
+		totalProgress += ppmDelta;
+		System.out.println("progress = " + totalProgress + "/1000000");
+	}
 }
