4b272f1f1d8f204ef397252216f49c62f81aa89c
Dodo <Dodo@LAPTOP-S32755C9>
2020-10-06 21:27:00 +0200

    Temprature- and water-controller implemented, need to be tested

--- wash/src/wash/control/TemperatureController.java
+++ wash/src/wash/control/TemperatureController.java
@@ -4,16 +4,65 @@ import actor.ActorThread;
 import wash.io.WashingIO;
 
 public class TemperatureController extends ActorThread<WashingMessage> {
+	private WashingIO io;
+	private int dt = 10000;
+	private ActorThread<WashingMessage> program;
 
-    // TODO: add attributes
+	public TemperatureController(WashingIO io) {
+		this.io = io;
+	}
 
-    public TemperatureController(WashingIO io) {
-        // TODO
-    }
+	private void sendAck() {
+		program.send(new WashingMessage(this, WashingMessage.ACKNOWLEDGMENT));
+	}
 
-    @Override
-    public void run() {
-        // TODO
+	@Override
+	public void run() {
 
-    }
+		try {
+			while (true) {
+
+				WashingMessage m = receiveWithTimeout(dt / Settings.SPEEDUP);
+				if (m != null) {
+					// The thread that sent the message
+					program = m.getSender();
+				}
+
+				while (program != null) {
+					switch (m.getCommand()) {
+					case WashingMessage.TEMP_IDLE: {
+						io.heat(false);
+						sendAck();
+						break;
+					}
+
+					case WashingMessage.TEMP_SET: {
+						double tempWeWant = m.getValue();
+						double lowerMargin = dt * 9.52 * 0.001;
+						double upperMargin = dt * 0.0478;
+						// måste kolla matten igen
+						if (io.getTemperature() - upperMargin < tempWeWant) {
+							io.heat(true);
+
+						} else {
+							io.heat(false);
+							break;
+						}
+						sendAck();
+					}
+					 default:
+                         System.out.println("Invalid command try again");
+
+					}
+				}
+
+			}
+
+		} catch (InterruptedException e) {
+			  // we don't expect this thread to be interrupted,
+            // so throw an error if it happens anyway
+            throw new Error();
+
+		}
+	}
 }
--- wash/src/wash/control/WaterController.java
+++ wash/src/wash/control/WaterController.java
@@ -4,15 +4,89 @@ import actor.ActorThread;
 import wash.io.WashingIO;
 
 public class WaterController extends ActorThread<WashingMessage> {
+	private WashingIO io;
+	private int dt = 5000;
+
+	private ActorThread<WashingMessage> program;
 
     // TODO: add attributes
 
     public WaterController(WashingIO io) {
-        // TODO
+    	this.io =io;
     }
+    
+
+	private void sendAck() {
+		program.send(new WashingMessage(this, WashingMessage.ACKNOWLEDGMENT));
+	}
+
 
     @Override
     public void run() {
-        // TODO
+    	
+    	
+    		try {
+    			while (true) {
+
+    				WashingMessage m = receiveWithTimeout(dt / Settings.SPEEDUP);
+    				if (m != null) {
+    					// The thread that sent the message
+    					program = m.getSender();
+    				}
+    				
+    				
+    				while(program != null) {
+    					switch (m.getCommand()) {
+						case WashingMessage.WATER_DRAIN: {
+							if (io.getWaterLevel() == 0) {
+	                			io.drain(false);
+	                			sendAck();
+	                		}
+	                		else {
+	                			io.drain(true);
+	                		}
+	                		io.fill(false);
+	                		break;
+							
+						}
+						case WashingMessage.WATER_FILL: {
+							if (io.getWaterLevel() < m.getValue()) {
+	                			io.fill(true);
+	                		}
+	                		else {
+	                			sendAck();
+	                			io.fill(false);
+	                		}
+	                		io.drain(false);
+	                		break;
+							
+						}
+						
+						case WashingMessage.WATER_IDLE: {
+							io.fill(false);
+	                		io.drain(false);
+	                		break;
+							
+						}
+						
+						 default:
+	                            System.out.println("Invalid command try again");
+					
+						}
+    					
+    					
+    					
+    				}
+    				
+    			}
+    		} catch (InterruptedException e) {
+    			  // we don't expect this thread to be interrupted,
+                // so throw an error if it happens anyway
+                throw new Error();
+
+    		}
+    	
+ 
+    
     }
 }
