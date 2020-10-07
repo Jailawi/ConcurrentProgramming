78ce302459a6147264bc8ee65cd829458a509964
Musti boii <mustafa.al-jailawi@mail.com>
2020-10-07 14:14:22 +0200

    temp funkar oftast?, visar alltid fel efter 30 min

--- wash/src/wash/control/TemperatureController.java
+++ wash/src/wash/control/TemperatureController.java
@@ -6,58 +6,64 @@ import wash.io.WashingIO;
 public class TemperatureController extends ActorThread<WashingMessage> {
 	private WashingIO io;
 	private int dt = 10;
-	private ActorThread<WashingMessage> program;
+	private WashingMessage program;
 
 	public TemperatureController(WashingIO io) {
 		this.io = io;
 	}
 
+	private void sendAck() {
+		program.getSender().send(new WashingMessage(this, WashingMessage.ACKNOWLEDGMENT));
+	}
 
 	@Override
 	public void run() {
 
 		try {
-			WashingMessage prev=null;
 			while (true) {
 
-				WashingMessage m = receiveWithTimeout(dt * 1000/ Settings.SPEEDUP);
+				WashingMessage m = receiveWithTimeout(dt * 1000 / Settings.SPEEDUP);
 				if (m != null) {
-					// The thread that sent the message
-					//program = m.getSender();
-					prev=m;
+					program = m;
 				}
 
-				if (prev != null) {
-					switch (prev.getCommand()) {
-					case WashingMessage.TEMP_IDLE: {
-						io.heat(false);
-						break;
-					}
-
-					case WashingMessage.TEMP_SET: {
-						double tempWeWant = prev.getValue();
-						double lowerMargin = 0.6;
-						double upperMargin = 0.3;
-						if (io.getTemperature() + upperMargin < tempWeWant-2) {
-							io.heat(true);
-							
-
-						} else if(io.getTemperature()+lowerMargin >tempWeWant ){
+				if (program != null) {
+					switch (program.getCommand()) {
+						case WashingMessage.TEMP_IDLE: {
 							io.heat(false);
-							
+							sendAck();
+							break;
 						}
-						break;
-					}
-					 
+
+						case WashingMessage.TEMP_SET: {
+							double tempWeWant = program.getValue();
+							double currentTemp = io.getTemperature();
+							double lowerMargin = dt * 0.000238 * (currentTemp - 20) - 0.085;
+							double upperMargin = 0.478;
+
+							if (currentTemp + lowerMargin < tempWeWant - 2 && currentTemp + upperMargin > tempWeWant) {
+								sendAck();
+							}
+
+							if (currentTemp + lowerMargin < tempWeWant - 2) {
+								io.heat(true);
+
+							} else if (currentTemp + upperMargin > tempWeWant) {
+								io.heat(false);
+
+							}
+							break;
+						}
+
 					}
 				}
 
 			}
 
 		} catch (InterruptedException e) {
-			  // we don't expect this thread to be interrupted,
-            // so throw an error if it happens anyway
-            throw new Error();
+			// we don't expect this thread to be interrupted,
+			// so throw an error if it happens anyway
+			throw new Error();
 
 		}
 	}
--- wash/src/wash/control/WashingProgram1.java
+++ wash/src/wash/control/WashingProgram1.java
@@ -1,6 +1,5 @@
 package wash.control;
 
-
 import actor.ActorThread;
 import wash.io.WashingIO;
 
@@ -27,59 +26,59 @@ public class WashingProgram1 extends ActorThread<WashingMessage> {
         this.water = water;
         this.spin = spin;
     }
-    
-    /*Lock the hatch, let water into the machine, heat to 40◦C, keep the temperature for
-30 minutes, drain, rinse 5 times 2 minutes in cold water, centrifuge for 5 minutes and unlock the hatch.
-While washing and rinsing the barrel should spin slowly, switching between left and right direction
-every minute. While centrifuging, the drain pump should run to evacuate excess water.*/
+
+    /*
+     * Lock the hatch, let water into the machine, heat to 40◦C, keep the
+     * temperature for 30 minutes, drain, rinse 5 times 2 minutes in cold water,
+     * centrifuge for 5 minutes and unlock the hatch. While washing and rinsing the
+     * barrel should spin slowly, switching between left and right direction every
+     * minute. While centrifuging, the drain pump should run to evacuate excess
+     * water.
+     */
 
     @Override
     public void run() {
         try {
-        	System.out.println("begin washingprogram1");
+            System.out.println("begin washingprogram1");
             // Lock the hatch
             io.lock(true);
-            
+
             System.out.println("Filling with Water");
-            water.send(new WashingMessage(this, WashingMessage.WATER_FILL,10));
+            water.send(new WashingMessage(this, WashingMessage.WATER_FILL, 10));
             WashingMessage ackWater = receive();
             water.send(new WashingMessage(this, WashingMessage.WATER_IDLE));
             System.out.println("setting SPIN_SLOW...");
             spin.send(new WashingMessage(this, WashingMessage.SPIN_SLOW));
             System.out.println("Heating to 40");
-            temp.send(new WashingMessage(this, WashingMessage.TEMP_SET,40));
-            //gör receive
-            //Keep the temp for 30min
+            temp.send(new WashingMessage(this, WashingMessage.TEMP_SET, 40));
+
+            // Keep the temp for 30min
             Thread.sleep(30 * 60000 / Settings.SPEEDUP);
+            WashingMessage ackTempSet = receive();
             temp.send(new WashingMessage(this, WashingMessage.TEMP_IDLE));
-            
-           System.out.println("Now we drain"); 
+            WashingMessage ackTempIdle = receive();
+            System.out.println("Now we drain");
             water.send(new WashingMessage(this, WashingMessage.WATER_DRAIN));
             WashingMessage ackDrain = receive();
-         
 
-            
             System.out.println("Rinsing");
-            for(int i=0; i<5;i++) {
-                water.send(new WashingMessage(this, WashingMessage.WATER_FILL,10));
+            for (int i = 0; i < 5; i++) {
+                water.send(new WashingMessage(this, WashingMessage.WATER_FILL, 10));
                 WashingMessage ackRinse = receive();
                 water.send(new WashingMessage(this, WashingMessage.WATER_IDLE));
                 spin.send(new WashingMessage(this, WashingMessage.SPIN_SLOW));
                 Thread.sleep(2 * 60000 / Settings.SPEEDUP);
                 water.send(new WashingMessage(this, WashingMessage.WATER_DRAIN));
-                 ackDrain=receive();
-                
+                ackDrain = receive();
+
             }
-            
-            
+
             spin.send(new WashingMessage(this, WashingMessage.SPIN_FAST));
             WashingMessage ackSpinFast = receive();
             water.send(new WashingMessage(this, WashingMessage.WATER_DRAIN));
-             ackDrain=receive();
+            ackDrain = receive();
             Thread.sleep(5 * 60000 / Settings.SPEEDUP);
-            
-         
-    
+
             io.lock(false);
 
             System.out.println("washing program 1 finished");
